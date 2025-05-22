4b1e1bb74590af523136cf87bf7777242e1a9b89
Lamont <viniciusmessias52@gmail.com>
2025-05-22 15:46:03 -0300

    Bubble Sort now is more readable :)

--- src/array/BubbleSort.java
+++ src/array/BubbleSort.java
@@ -5,46 +5,47 @@ package array;
 public class BubbleSort {
     public static void main(String[] args) {
 
-        int[] numbers = new int[]{7, 5, 2, 0, 3, 9, 8, 6, 1, 4};
+        int[] ascending = new int[]{7, 5, 2, 0, 3, 9, 8, 6, 1, 4};
+        int[] descending = new int[]{7, 5, 2, 0, 3, 9, 8, 6, 1, 4};
 
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = 0; j < numbers.length - 1 - i; j++) {
-                if (numbers[j] > numbers[j + 1]) {
-                    int temp = numbers[j];
-                    numbers[j] = numbers[j + 1];
-                    numbers[j + 1] = temp;
+        for (int i = 0; i < ascending.length - 1; i++) {
+            for (int j = 0; j < ascending.length - 1 - i; j++) {
+                if (ascending[j] > ascending[j + 1]) {
+                    int temp = ascending[j];
+                    ascending[j] = ascending[j + 1];
+                    ascending[j + 1] = temp;
                 }
             }
         }
+
+        for (int i = 0; i < descending.length - 1; i++) {
+            for (int j = 0; j < descending.length - 1 - i; j++) {
+                if (descending[j] < descending[j + 1]) {
+                    int temp = descending[j];
+                    descending[j] = descending[j + 1];
+                    descending[j + 1] = temp;
+                }
+            }
+        }
+
         System.out.println("##########BUBBLE SORT##########");
         System.out.println("=======---ascending---=======");
-        for (int i = 0; i < numbers.length; i++) {
-            if (i < numbers.length - 1) {
-                System.out.print("[" + numbers[i] + "]");
+        for (int i = 0; i < ascending.length; i++) {
+            if (i < ascending.length - 1) {
+                System.out.print("[" + ascending[i] + "]");
             } else {
-                System.out.print("[" + numbers[i]);
+                System.out.print("[" + ascending[i]);
             }
         }
         System.out.println("]");
         System.out.println("=======---ascending---=======");
-        System.out.println();
-        numbers = new int[]{7, 5, 2, 0, 3, 9, 8, 6, 1, 4};
 
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = 0; j < numbers.length - 1 - i; j++) {
-                if (numbers[j] < numbers[j + 1]) {
-                    int temp = numbers[j];
-                    numbers[j] = numbers[j + 1];
-                    numbers[j + 1] = temp;
-                }
-            }
-        }
         System.out.println("=======---descending---=======");
-        for (int i = 0; i < numbers.length; i++) {
-            if (i < numbers.length - 1) {
-                System.out.print("[" + numbers[i] + "]");
+        for (int i = 0; i < descending.length; i++) {
+            if (i < descending.length - 1) {
+                System.out.print("[" + descending[i] + "]");
             } else {
-                System.out.print("[" + numbers[i]);
+                System.out.print("[" + descending[i]);
             }
         }
         System.out.println("]");
