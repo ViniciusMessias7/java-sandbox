ed1d8dd4550fc1ea7a5bd0f98f1236936f5d9247
Lamont <viniciusmessias52@gmail.com>
2025-05-30 14:52:31 -0300

    feat: new exercises

---
+++ src/exercises/exercise01/SumComparison.java
@@ -0,0 +1,29 @@
+package exercises.exercise01;
+
+import java.util.Scanner;
+
+// Create an algorithm that reads the values of A, B, and C.
+// Then prints the sum of A and B to the screen and shows if the sum is less than C.
+public class SumComparison {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Enter an integer 'a': ");
+        int valueA = userInput.nextInt();
+
+        System.out.print("Enter an integer 'b': ");
+        int valueB = userInput.nextInt();
+
+        System.out.print("Enter an integer 'c': ");
+        int valueC = userInput.nextInt();
+
+        userInput.close();
+
+        int sumOfAAndB = (valueA + valueB);
+        System.out.println("The sum between 'a' + 'b' is " + sumOfAAndB + ".");
+
+        if (sumOfAAndB < valueC) {
+            System.out.println("The sum of both is less than 'c': " + valueC + ".");
+        }
+    }
+}
---
+++ src/exercises/exercise02/EvenOrOdd.java
@@ -0,0 +1,33 @@
+package exercises.exercise02;
+
+import java.util.Scanner;
+
+// Create an algorithm to receive any number and print on the screen whether the
+// number is even or odd, positive, neutral or negative.
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Enter any number: ");
+        int numberEntered = userInput.nextInt();
+
+        userInput.close();
+
+        String message;
+        if (numberEntered % 2 == 0) {
+            message = "The number you entered '" + numberEntered + "' is even";
+        } else {
+            message = "The number you entered '" + numberEntered + "' is odd";
+        }
+
+        if (numberEntered > 0) {
+            message += " and positive.";
+        } else if (numberEntered == 0) {
+            message += " and neutral.";
+        } else {
+            message += " and negative.";
+        }
+
+        System.out.println(message);
+    }
+}
